3345cd7e8cf9b00e9ec26da6706378bac311ef80
Pirir211 <enmanuelcanel.15@gmail.com>
2020-10-03 18:31:04 -0600

    mod1

--- Tarea3_Allan_Pirir1055919/MyForm.h
+++ Tarea3_Allan_Pirir1055919/MyForm.h
@@ -34,6 +34,15 @@ namespace Tarea3AllanPirir1055919 {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::ListBox^ listBox1;
+	protected:
+	private: System::Windows::Forms::ListBox^ listBox2;
+	private: System::Windows::Forms::ListBox^ listBox3;
+	private: System::Windows::Forms::ListBox^ listBox4;
+	private: System::Windows::Forms::ListBox^ listBox5;
+	private: System::Windows::Forms::ListBox^ listBox6;
+	private: System::Windows::Forms::ListBox^ listBox7;
+	private: System::Windows::Forms::Button^ button1;
 
 	private:
 		/// <summary>
@@ -48,13 +57,101 @@ namespace Tarea3AllanPirir1055919 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox2 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox3 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox4 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox5 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox6 = (gcnew System::Windows::Forms::ListBox());
+			this->listBox7 = (gcnew System::Windows::Forms::ListBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->ItemHeight = 16;
+			this->listBox1->Location = System::Drawing::Point(26, 160);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(120, 196);
+			this->listBox1->TabIndex = 0;
+			// 
+			// listBox2
+			// 
+			this->listBox2->FormattingEnabled = true;
+			this->listBox2->ItemHeight = 16;
+			this->listBox2->Location = System::Drawing::Point(166, 160);
+			this->listBox2->Name = L"listBox2";
+			this->listBox2->Size = System::Drawing::Size(120, 196);
+			this->listBox2->TabIndex = 1;
+			// 
+			// listBox3
+			// 
+			this->listBox3->FormattingEnabled = true;
+			this->listBox3->ItemHeight = 16;
+			this->listBox3->Location = System::Drawing::Point(308, 160);
+			this->listBox3->Name = L"listBox3";
+			this->listBox3->Size = System::Drawing::Size(120, 196);
+			this->listBox3->TabIndex = 2;
+			// 
+			// listBox4
+			// 
+			this->listBox4->FormattingEnabled = true;
+			this->listBox4->ItemHeight = 16;
+			this->listBox4->Location = System::Drawing::Point(444, 160);
+			this->listBox4->Name = L"listBox4";
+			this->listBox4->Size = System::Drawing::Size(120, 196);
+			this->listBox4->TabIndex = 3;
+			// 
+			// listBox5
+			// 
+			this->listBox5->FormattingEnabled = true;
+			this->listBox5->ItemHeight = 16;
+			this->listBox5->Location = System::Drawing::Point(580, 160);
+			this->listBox5->Name = L"listBox5";
+			this->listBox5->Size = System::Drawing::Size(120, 196);
+			this->listBox5->TabIndex = 4;
+			// 
+			// listBox6
+			// 
+			this->listBox6->FormattingEnabled = true;
+			this->listBox6->ItemHeight = 16;
+			this->listBox6->Location = System::Drawing::Point(732, 160);
+			this->listBox6->Name = L"listBox6";
+			this->listBox6->Size = System::Drawing::Size(120, 196);
+			this->listBox6->TabIndex = 5;
+			// 
+			// listBox7
+			// 
+			this->listBox7->FormattingEnabled = true;
+			this->listBox7->ItemHeight = 16;
+			this->listBox7->Location = System::Drawing::Point(877, 160);
+			this->listBox7->Name = L"listBox7";
+			this->listBox7->Size = System::Drawing::Size(120, 196);
+			this->listBox7->TabIndex = 6;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(58, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(98, 36);
+			this->button1->TabIndex = 7;
+			this->button1->Text = L"Empezar";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(813, 710);
+			this->ClientSize = System::Drawing::Size(1075, 537);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->listBox7);
+			this->Controls->Add(this->listBox6);
+			this->Controls->Add(this->listBox5);
+			this->Controls->Add(this->listBox4);
+			this->Controls->Add(this->listBox3);
+			this->Controls->Add(this->listBox2);
+			this->Controls->Add(this->listBox1);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
 			this->ResumeLayout(false);
